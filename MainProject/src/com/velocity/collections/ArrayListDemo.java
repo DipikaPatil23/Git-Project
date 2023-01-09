ce519788cc8a3bb58e091d3823d4e0073893f822
DipikaPatil <patildipika1999@gmail.com>
2023-01-09 12:09:02 +0530

    first commit

---
+++ MainProject/src/com/velocity/collections/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("yash");
+		al.add("neha");
+		al.add("ankit");
+		al.add("vikas");
+		System.out.println(al);
+		
+	}
+}
